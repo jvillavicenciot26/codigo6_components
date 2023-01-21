f6405af96b0dcd0cc806cf06e0b2b1019862adb2
Juan Manuel <jvillavicenciot26@outlook.com.pe>
2023-01-20 23:06:53 -0500

    Inicio de ListView

--- lib/Pages/list_page.dart
+++ lib/Pages/list_page.dart
@@ -7,6 +7,23 @@ class ListViewPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("List View Page"),
       ),
+      // body: ListView(
+      //   children: [
+      //     Container(
+      //       color: Colors.blue,
+      //       width: 200,
+      //       height: 200,
+      //     ),
+      //   ],
+      body: ListView.builder(
+        itemCount: 3, //Especifica la cantidad de items que manejara el listview
+        itemBuilder: (BuildContext context, int entero) {
+          print(entero);
+          return Text(
+            "Hola",
+          ); //Si no se especifica un itemCount, se generara infinitamente este widget
+        },
+      ),
     );
   }
 }
